92af9a32134a2ca6392cc4ca92f6b24ae5765aa1
leandrocgsi <leandrocgsi@gmail.com>
2018-11-08 15:21:52 -0200

    Adding more tests

--- 01_Calculator/src/test/java/br/com/erudio/request/converters/TestNumberConverter.java
+++ 01_Calculator/src/test/java/br/com/erudio/request/converters/TestNumberConverter.java
@@ -27,6 +27,8 @@ public class TestNumberConverter {
         assertEquals(new BigDecimal("3.5"), NumberConverter.sum("2,5", "1,0"));
         assertEquals(new BigDecimal("2"), NumberConverter.sum("-2", "4"));
         assertEquals(new BigDecimal("6.4"), NumberConverter.sum("1.3", "5.1"));
+        assertEquals(new BigDecimal("9.22000005"), NumberConverter.sum("4.22000005", "5.0"));
+        assertEquals(new BigDecimal("6.11100000000004"), NumberConverter.sum("2.11100000000004", "4"));
     }
     
     @Test(expected = Exception.class)
